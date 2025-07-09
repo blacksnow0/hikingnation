efdab15ec960e6dc0b8d8f700c76bcde0f6e15fe
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2025-07-09 09:58:58 +0530

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FiMenu, FiX } from "react-icons/fi";
 import logo from "../assets/logo.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
+
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="fixed top-0 w-full z-50 bg-white/20 backdrop-blur shadow-md px-4 py-4">
+    <nav
+      className={`fixed top-0 w-full z-50 backdrop-blur shadow-md px-4 py-4 ${
+        isHome ? "bg-white/20" : "bg-green-900/80"
+      }`}
+    >
       <div className="flex items-center justify-between max-w-6xl mx-auto">
         <Link to="/" className="flex items-center gap-2 hover:text-[#1b5e20]">
           <img
@@ -33,7 +41,9 @@ export default function Navbar() {
 
         <button
           onClick={toggleMenu}
-          className="md:hidden focus:outline-none text-[#1f2937]"
+          className={`md:hidden focus:outline-none ${
+            isHome ? " text-[#1f2937]" : "text-white"
+          }`}
         >
           {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
         </button>
@@ -42,7 +52,7 @@ export default function Navbar() {
       {isOpen && (
         <div
           className="md:hidden px-4 mt-2 space-y-2 text-sm font-semibold tracking-wide
-               transition-all duration-300 ease-in-out animate-slide-fade"
+               transition-all duration-300 ease-in-out animate-slide-fade "
         >
           <Link
             to="/"
